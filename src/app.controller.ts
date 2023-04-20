5deb9e281bf22168b2f81fab05a0ecf1d1d924cb
goduer <526140067@qq.com>
2023-04-20 18:51:31 +0800

    feat: 接入swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags, ApiOperation, ApiProperty } from '@nestjs/swagger';
 
+class CreateIndexDto {
+  @ApiProperty({ description: '主键' })
+  readonly id: number;
+}
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiTags('首页')
+  @ApiOperation({ summary: '首页测试接口' })
   @Get()
-  getHello(): string {
+  getHello(@Body() id: CreateIndexDto): string {
+    console.log(
+      '🚀 ~ file: app.controller.ts:16 ~ AppController ~ getHello ~ id:',
+      id,
+    );
     return this.appService.getHello();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api'); // 设置全局路由前缀
+
+  // 设置swagger文档
+  const config = new DocumentBuilder()
+    .setTitle('管理后台')
+    .setDescription('管理后台接口文档')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(9090);
-  console.log('listen: http://127.0.0.1:9090');
+  console.log('🚀 listen: http://127.0.0.1:9090');
 }
 bootstrap();
